8e32264c2c48444a6f263d0b4fe37c4d726fdfb5
Farid Abdi <bngfm97@gmail.com>
2024-05-17 17:09:42 +0100

    api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBcJVol4iIzej3tRUvD09ooTmGf6IM7CJw",
-    authDomain: "kanbanfire-748f6.firebaseapp.com",
-    projectId: "kanbanfire-748f6",
-    storageBucket: "kanbanfire-748f6.appspot.com",
-    messagingSenderId: "98763122799",
-    appId: "1:98763122799:web:e9c703038688c90fe8859a",
-    measurementId: "G-H23WQ4R50H"
+    apiKey: "xxxxx",
+    authDomain: "xxxxx",
+    projectId: "xxxxxx",
+    storageBucket: "xxxxxxx",
+    messagingSenderId: "xxxxxxxx",
+    appId: "xxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxx"
   }
 };
